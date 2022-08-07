4d4e1684ae94f09f4380028a5b62f34b5b1f00ad
Terrence_AA <terryterrence200@gmail.com>
2022-08-07 20:41:05 +0300

    ---

---
+++ RemoteServerApplication2/RemoteServerApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using PTSLibrary2;
+using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+
+namespace RemoteServerApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpChannel channel = new HttpChannel(50000);
+            ChannelServices.RegisterChannel(channel, false);
+           RemotingConfiguration.RegisterWellKnownServiceType(typeof(PTSLibrary2.PTSAdminFacade),"PTSAdminFacade", WellKnownObjectMode.Singleton);
+            Console.WriteLine("press the enter key to terminate the server");
+            Console.ReadLine();
+
+        }
+    }
+}
